b5e40e194c9c12f4345322f82513edbc8374299d
jcjohnson92 <37008252+jcjohnson92@users.noreply.github.com>
2024-05-30 22:19:01 +0000

    Recently completed challenges

---
+++ CodeWars/reversedstring.py
@@ -0,0 +1,17 @@
+'''
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+
+
+
+'''
+
+def solution(string):
+    return string[::-1]
+
+
+
+x=solution("world")
+print(x)
